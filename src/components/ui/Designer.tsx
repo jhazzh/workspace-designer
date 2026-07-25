283ef48d4ca500bbf4fcd17ae1baf888c78a15d2
jhazzh <83843786+jhazzh@users.noreply.github.com>
2026-07-25 15:21:55 +0800

    Layer the stage above the side panels

--- src/components/ui/Designer.tsx
+++ src/components/ui/Designer.tsx
@@ -78,7 +78,7 @@ export function Designer() {
       </aside>
 
       {/* stage */}
-      <section className="relative order-1 min-h-0 flex-1 lg:order-2">
+      <section className="relative order-1 min-h-0 flex-1 lg:order-2 z-1">
         <WorkspaceCanvas />
 
         <div className="pointer-events-none absolute inset-x-0 top-0 flex max-w-full justify-center p-3">
